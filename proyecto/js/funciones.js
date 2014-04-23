489204c26dd9a25f903919fcf38c8f3b99ef695c
raftovper <tovarpereira@gmail.com>
2014-04-23 19:14:17 +0200

    Prueba
    Primer commit, solo prueba

---
+++ proyecto/js/funciones.js
@@ -0,0 +1,26 @@
+$(inicio);
+
+function inicio() {
+	
+	//Obtenemos la altura del dispositivo para establecer el alto del mapa
+	var mapHeight = $(window).height() * 0.65;
+	
+	$('#map_canvas').height(mapHeight);
+
+
+	$('#map').on("pageshow", null, function() {
+		$('#map_canvas').gmap('addMarker', {
+			'position' : '37.392864, -5.988125',
+			'bounds' : true
+		}).click(function() {
+			$('#map_canvas').gmap('openInfoWindow', {
+				'content' : 'Hello World!'
+			}, this);
+		});
+	});
+
+}
+
+window.addEventListener("load", function() {
+	FastClick.attach(document.body);
+});
